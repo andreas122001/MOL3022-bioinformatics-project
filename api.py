f6bfda6f66049a2269968295007870b0019c89af
andreas122001 <andreaswinje@hotmail.com>
2024-02-20 16:14:11 +0100

    implemented a basic backend (fastapi)

---
+++ api.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoModel
+from typing import List, Dict
+import asyncio
+
+from concurrent.futures import ProcessPoolExecutor
+
+_threshold = 0.7
+_model = None
+
+class FastaData(BaseModel):
+    header: str
+    sequence: str
+    threshold: float = _threshold
+
+app = FastAPI()
+
+def init_model():
+    global _model
+    _model = None
+    # model = AutoModel.from_pretrained(...)
+
+# pool = ProcessPoolExecutor(max_workers=1, initializer=init_model)
+
+
+async def model_predict(header: str, sequence: str) -> Dict[float,float]:
+    await asyncio.sleep(3.)
+    return {
+        'no_sp': 0.2,
+        'sp': 0.8,
+    }
+
+@app.get("/hello")
+def hello_world():
+    print("Hello, World!")
+    return {
+        "message": "Hello, World!"
+    }
+
+@app.post("/inference")
+async def post_inference(data: FastaData):
+    print(data)
+
+    result = await model_predict(data.header, data.sequence)
+
+    return {
+        'positive': result['sp'],
+        'negative': result['no_sp'],
+        'predicted': 'Signal peptide' if result['sp'] > data.threshold
+            else 'No signal peptide' if result['no_sp'] > data.threshold
+            else 'Uncertain'
+    }
+
+
