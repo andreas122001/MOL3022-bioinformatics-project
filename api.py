5f18d0520d4593c86ea90d643e578dd7f408b2ab
andreas122001 <andreaswinje@hotmail.com>
2024-03-12 16:04:01 +0100

    added CORS config to backend

--- api.py
+++ api.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel
 from transformers import AutoModel
 from typing import List, Dict
 import asyncio
+from fastapi.middleware.cors import CORSMiddleware
 
 from concurrent.futures import ProcessPoolExecutor
 
 _threshold = 0.7
 _model = None
+
 
 class FastaData(BaseModel):
     header: str
@@ -15,6 +17,20 @@ class FastaData(BaseModel):
     threshold: float = _threshold
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def init_model():
     global _model
