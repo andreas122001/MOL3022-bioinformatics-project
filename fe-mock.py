94e6d8f1cdb8af8dd7c5f2f23968012f413b3fae
andreas122001 <andreaswinje@hotmail.com>
2024-03-13 00:21:20 +0100

    updated fe-mock.py

--- fe-mock.py
+++ fe-mock.py
@@ -2,6 +2,7 @@ import aiohttp
 import asyncio
 import time
 import sys
+from exceptiongroup import catch
 import numpy as np
 
 n = int(sys.argv[1]) if len(sys.argv) > 1 else 100
@@ -9,24 +10,21 @@ n = int(sys.argv[1]) if len(sys.argv) > 1 else 100
 # Define your API endpoint
 url = "http://localhost:8080/inference"
 
-data = None
-with open("./data/examples.fasta") as f:
-    data = f.read()
-    data = data.split(">")
-    data = [d for d in data if d]
-    data = [{ 'header': ">"+d.split("\n")[0], 'sequence': d.split("\n")[1] } for d in data]
-
-# Create a list of JSON payloads
-if not data:
-    payloads = [
+try:
+    data = None
+    with open("./data/examples.fasta") as f:
+        data = f.read()
+        data = data.split(">")
+        data = [d for d in data if d]
+        data = [{ 'header': ">"+d.split("\n")[0], 'sequence': d.split("\n")[1] } for d in data]
+    payloads = np.random.choice(data, n)
+except:
+        payloads = [
         {
             "header": ">EUKARYA",
             "sequence": "MAPTLFQKLFSKRTGLGAPGRDARDPDCGFSWPLPEFDPSQIRLIVYQDCERRGRNVLFDSSVKRRNEDI",
         }
-        # Add more payloads as needed
     ]*n
-else:
-    payloads = np.random.choice(data, n)
 
 async def send_request(session, payload):
     start_time = time.time()
